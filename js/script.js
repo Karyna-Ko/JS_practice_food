27eb30a851c02affb9f94380ca846e5a41129303
Karyna-Ko <karina.korshak@gmail.com>
2022-07-07 11:51:46 +0300

    added rest(operator) parameters

--- js/script.js
+++ js/script.js
@@ -148,12 +148,13 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
 // Classes for cards
 
     class MenuCard {
-        constructor (src, alt, title, description, price, parentSelector) {
+        constructor (src, alt, title, description, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.description = description;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 29;
             this.changeToUAH();
@@ -165,8 +166,14 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item',
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.description}</div>
@@ -175,7 +182,6 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `;
             this.parent.append(element);
         }
